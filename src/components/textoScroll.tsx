ef0434117506090961591fde0270ea83f0ce9eb2
ChrisDestroyer <64537705+ChrisDestroyer@users.noreply.github.com>
2026-09-23 18:21:26 -0600

    texto - scroll corregido

--- src/components/textoScroll.tsx
+++ src/components/textoScroll.tsx
@@ -4,9 +4,9 @@ export default function TextoScroll() {
   return (
     <div className="scroll-container">
       <div className="scroll-text">
-        Tío Ben Pino Suárez - Menú Healthy * Nuevas ensaladas y bowl teriyaki disponibles *
-        Tío Ben Pino Suárez - Menú Healthy * Nuevas ensaladas y bowl teriyaki disponibles *
-        Tío Ben Pino Suárez - Menú Healthy * Nuevas ensaladas y bowl teriyaki disponibles *
+        Boutique Pino Suárez - Boutique Pino Suárez * Boutique Pino Suárez y Boutique Pino Suárez *
+        Boutique Pino Suárez - Boutique Pino Suárez * Boutique Pino Suárez y Boutique Pino Suárez *
+        Boutique Pino Suárez - Boutique Pino Suárez * Boutique Pino Suárez y Boutique Pino Suárez *
       </div>
     </div>
   );
